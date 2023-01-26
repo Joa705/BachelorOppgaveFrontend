4ff2e50ffc40b57e0e3a91135e226d7d104d7aff
Joachim <ja@indicosys.com>
2023-01-26 15:12:34 +0100

    useauth example

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-
+import { UseAuth } from "../functions/authentication";
 
 function Post() {
     const {id} = useParams()
-
+    const {admin, userName} = UseAuth();
     return (
      <div>
          <h1>Her vises en enkel post</h1>
          <h2>Dette er for eksempel post med id: {id}</h2>
+
+         {admin? 
+            <p>DU er en admin {userName}</p> 
+            :
+            <p>DU er ikke admin</p>
+        }
      </div>   
     )
 
