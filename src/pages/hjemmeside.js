b7186b27e59b06ead131192c8cf990b4fcc01602
Adrian Jakub Adamski <jakob.aa@hotmail.com>
2023-02-09 22:18:25 +0100

    added illustration bar

--- src/pages/hjemmeside.js
+++ src/pages/hjemmeside.js
@@ -1,6 +1,13 @@
 import "../styling/hjemmeside.css";
 import React from "react";
 import asplan from "../illustrasjon.png";
+import il1 from "../1.png";
+import il2 from "../2.png";
+import il3 from "../3.png";
+
+import il4 from "../4.png";
+import il5 from "../5.png";
+import il6 from "../6.png";
 import viak from "../asplan_viak_1.jpg";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -33,15 +40,21 @@ function Hjemmeside() {
         <div className="container">
           <div className="row">
             <div className="col-sm">
-              Enkel å komme i gang. Ingen registrering. Bare logg deg inn med
-              Azure AD
+            <div className="image1"
+          style={{ backgroundImage: `url(${il4})` }}></div>
+              Det er fort å komme i gang med, vi krever ingen registrering. Logg inn på en-to-tre med
+              Azure AD.
             </div>
             <div className="col-sm">
-              Registrer tilbakemelding av din opplevelse av våre systemer. Enten
-              det er ris, ros eller forslag til ny funksjonalitet
+            <div className="image2"
+          style={{ backgroundImage: `url(${il5})` }}></div>
+              Registrer en tilbakemelding av din opplevelse av våre tjenester. Velg
+              mellom ris, ros eller forbedringstips.
             </div>
             <div className="col-sm">
-              Du vil straks få svar av en av våre administratorer
+            <div className="image3"
+          style={{ backgroundImage: `url(${il6})` }}></div>
+              Sånn, da er det gjort! Vi skal gå gjennom feedbacken din. Du vil straks få svar av oss. Vi setter pris på det!
             </div>
           </div>
         </div>
